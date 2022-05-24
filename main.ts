db8e680a0eb819f79a6a13cef4b38abed8004e97
Anton Kondratev <kondratev@elma-bpm.com>
2022-05-24 17:46:57 +0400

    added first version

--- main.ts
+++ main.ts
@@ -1,21 +1,12 @@
 import * as ts from "typescript";
 import * as fs from "fs";
 import * as path from "path";
-import {type} from "os";
+import {log} from "util";
 console.clear()
 console.log('start')
-// const {ts} = require("typescript")
-// const fs = require("fs")
-// const path = require("path")
-// const serialize = require('serialize-to-js')
 
 const filename = "ttt213.ts";
-const code = `export interface ItemRef<I extends BaseItem<any>> extends RefItem {
-    /**
-     * Запрос цели ссылки
-     */
-    fetch(): Promise<I>;
-}`;
+const code = `const i = 'num'`;
 
 const sourceFile = ts.createSourceFile(
     filename, code, ts.ScriptTarget.Latest
@@ -31,21 +22,15 @@ type ObjectKey = keyof typeof objectAST;
 const myVar = 'statements' as ObjectKey;
 // console.log('myVar')
 // console.log(myVar)
-// console.log(objectAST[myVar]);
-console.log('---')
-console.log(ts.isVariableDeclaration)
+// console.log(Array.isArray(objectAST[myVar]) );
+// console.log('---')
+// console.log(ts.isVariableDeclaration)
+let i: any = sourceFile['statements'][0]
+let n: any = ts.SyntaxKind[i.kind]
+// console.log(n)
 
+let json: any = {}
 
-
-
-
-
-
-fs.writeFile(path.resolve(__dirname, 'ast-main.json'), String(objectAST[myVar]['end']), function(error){
-
-    if(error) throw error; // если возникла ошибка
-    console.log("Асинхронная запись файла завершена. Содержимое файла:");
-});
 // console.log('+++)))')
 function printRecursiveFrom(
     node: ts.Node, indentLevel: number, sourceFile: ts.SourceFile
@@ -54,26 +39,45 @@ function printRecursiveFrom(
     const syntaxKind = ts.SyntaxKind[node.kind];
     const nodeText = node.getText(sourceFile);
 
-    console.log(syntaxKind)
+    // console.log(syntaxKind)
     // console.log(`${indentation}${syntaxKind}: ${nodeText}`);
+    // json[syntaxKind] = `${nodeText} + ${node.getStart(sourceFile)}`
+    // console.log(node.getChildren(sourceFile))
+    // json[indentLevel] = `${syntaxKind} + ${nodeText}`
+    // console.log(syntaxKind)
+    if (0 === indentLevel) {
 
+    }
     node.getChildren(sourceFile).forEach(child =>
         printRecursiveFrom(child, indentLevel + 1, sourceFile)
     );
 }
 
-// printRecursiveFrom(sourceFile, 0, sourceFile);
+printRecursiveFrom(sourceFile, 0, sourceFile);
 
-// let array: string[] = ['name', 'text']
-//
-// const obj = {
-//     name: 'Tom1',
-//     text: 'Chile',
-// };
-//
-// type ObjectKey = keyof typeof obj;
-//
-// const myVar = array[1] as ObjectKey;
-// console.log(myVar)
-// console.log(obj[myVar]);
+setTimeout(() => {
+    // console.log(json)
+    setJson()
+}, 500)
 
+function setJson(): void {
+    fs.writeFile(path.resolve(__dirname, 'ast-main.json'), JSON.stringify(json), function(error){
+
+        if(error) throw error; // если возникла ошибка
+        console.log("Асинхронная запись файла завершена. Содержимое файла:");
+    });
+}
+
+`
+SourceFile
+SyntaxList
+FirstStatement
+VariableDeclarationList
+ConstKeyword
+SyntaxList
+VariableDeclaration
+Identifier
+FirstAssignment
+StringLiteral
+EndOfFileToken
+`
